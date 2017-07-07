93fe90cf454607f5e80a8b5884ee909fcb15b0ae
KiselevNikolayGit <neondoctors@gmail.com>
2017-07-07 11:14:52 +0300

    Загрузка работает!

--- PASTLEME.lua
+++ PASTLEME.lua
@@ -1,11 +1,8 @@
--- return {1,0, 1,-1, -1,1}, "fill", "#cacaba"
--- return {1,0, 1,-1, -1,1}, %ImageData%
--- return {1,0, 1,-1, -1,1}, "image.png", 15, 15, 5, 5
-
+-- ORIGINALS --
 function love.PMALopen(filename)
   container = love.filesystem.load(filename)
-  ... = container()
-  pmal = {...}
+  pmal = {container()}
+  type = "unknown"
   sizew = nil
   sizeh = nil
   offsetx = nil
@@ -16,21 +13,46 @@ function love.PMALopen(filename)
   style = pmal[2]
   if style == "fill" or style == "line" then
     color = pmal[3]
+    type = "simple"
   else
     image = love.graphics.newImage(style)
     style = nil
-  end
-  if #pmal >= 4 then
-    sizew = pmal[3]
-    sizeh = pmal[4]
-    if #pmal = 6 then
-      offsetx = pmal[5]
-      offsety = pmal[6]
+    type = "image"
+    if #pmal >= 4 then
+        sizew = pmal[3]
+        sizeh = pmal[4]
+        if #pmal == 6 then
+        offsetx = pmal[5]
+        offsety = pmal[6]
+        else
+        offsetx = sizew / 2
+        offsety = sizeh / 2
+        end
     else
-      offsetx = sizew / 2
-      offsety = sizeh / 2
+        sizew = image:getWidth()
+        sizeh = image:getHeight()
+        offsetx = sizew / 2
+        offsety = sizeh / 2
     end
-  else
-    --
   end
+  pmal = {}
+  pmal.t = type
+  print("type:    ", type)
+  pmal.p = points
+  print("points:  ", points)
+  pmal.s = style
+  print("style:   ", style)
+  pmal.c = color
+  print("color:   ", color)
+  pmal.i = image
+  print("image:   ", image)
+  pmal.w = sizew
+  print("sizew:   ", sizew)
+  pmal.h = sizeh
+  print("sizeh:   ", sizeh)
+  pmal.ox = offsetx
+  print("offsetx: ", offsetx)
+  pmal.oy = offsety
+  print("offsety: ", offsety)
+  return pmal
 end
